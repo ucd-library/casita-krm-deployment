bda2b717f6d8f4c5ab4aa6f33dcf53aa73e210cc
Justin Merz <jrmerz@gmail.com>
2020-10-02 12:07:47 -0700

    switching to dev krm

--- config.sh
+++ config.sh
@@ -15,7 +15,7 @@ APP_VERSION=v0.0.1
 # Repository tags/branchs
 # Tags should always be used for production deployments
 # Branches can be used for development deployments
-KRM_TAG=main
+KRM_TAG=dev
 CASITA_TASKS_TAG=main
 
 # Docker
